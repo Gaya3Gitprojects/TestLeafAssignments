5877f6ce61c396a2d62f0510f932bbe68582bd7e
Gaya3Gitprojects <gaya3.parthiban1@gmail.com>
2026-06-23 14:26:17 -0500

    Add files via upload

---
+++ BOsession2.js
@@ -0,0 +1,12 @@
+function oddOrEven1() {  
+   for (let a = 0; a <=10; a++)
+    {
+   if (a % 2 === 0) {
+        console.log(a,"is even");
+    } 
+    else {
+        console.log(a,"is odd");
+    }
+}
+}
+oddOrEven1();
